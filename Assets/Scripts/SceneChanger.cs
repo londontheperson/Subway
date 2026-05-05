f298355a72cf048b3325da80427413124c6701f2
NatrlDizastr-hub <kaykat1355@gmail.com>
2026-05-04 22:46:51 -0400

    update!!!!
    5/4 oh my gerb

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-public string sceneToLoad;
+    public string sceneToLoad;
+
+
+    public void ChangeSceneNow()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
 }
